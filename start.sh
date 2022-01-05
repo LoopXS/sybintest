f2a13448debe4ff5a5c212eb74918d521232c9d4
LoopXS <91501744+LoopXS@users.noreply.github.com>
2022-01-05 16:19:54 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/LoopXS/sybintest /sybintest
+  git clone https://github.com/LoopXS/sybintest /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/LoopXS/sybintest -b $BRANCH /sybintest
+  git clone https://github.com/LoopXS/sybintest -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
