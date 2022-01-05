7dee309d3697bed8280491d5b97c313f86c09721
LoopXS <91501744+LoopXS@users.noreply.github.com>
2022-01-05 16:56:47 +0330

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -63,18 +63,18 @@ async def start(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(f"Play", callback_data='help_play'),
-                        InlineKeyboardButton(f"Settings", callback_data=f"help_settings"),
-                        InlineKeyboardButton(f"Recording", callback_data='help_record'),
+                        InlineKeyboardButton(f"✗ ᴘʟᴀʏ ✗", callback_data='help_play'),
+                        InlineKeyboardButton(f"✗ ꜱᴇᴛᴛɪɴɢꜱ ✗", callback_data=f"help_settings"),
+                        InlineKeyboardButton(f"✗ ʀᴇᴄᴏʀᴅɪɴɢ ✗", callback_data='help_record'),
                     ],
                     [
-                        InlineKeyboardButton("Scheduling", callback_data="help_schedule"),
-                        InlineKeyboardButton("Controling", callback_data='help_control'),
-                        InlineKeyboardButton("Admins", callback_data="help_admin"),
+                        InlineKeyboardButton("✗ ꜱᴄʜᴇᴅᴜʟɪɴɢ ✗", callback_data="help_schedule"),
+                        InlineKeyboardButton("✗ ᴄᴏɴᴛʀᴏʟɪɴɢ ✗", callback_data='help_control'),
+                        InlineKeyboardButton("✗ ᴀᴅᴍɪɴꜱ ✗", callback_data="help_admin"),
                     ],
                     [
-                        InlineKeyboardButton(f"Misc", callback_data='help_misc'),
-                        InlineKeyboardButton("Close", callback_data="close"),
+                        InlineKeyboardButton(f"✗ ᴍɪꜱᴄ ✗", callback_data='help_misc'),
+                        InlineKeyboardButton("✗ ᴄʟᴏꜱᴇ ✗", callback_data="close"),
                     ],
                 ]
                 )
@@ -121,7 +121,7 @@ async def start(client, message):
                         k=d    
                     f.append(InlineKeyboardButton(text=f"{k}",callback_data=f"sch_month_{year_}_{month}_{d}"))
                 button.append(f)
-            button.append([InlineKeyboardButton("Close", callback_data="schclose")])
+            button.append([InlineKeyboardButton("✗ ᴄʟᴏꜱᴇ ✗", callback_data="schclose")])
             await msg.edit(f"Choose the day of the month you want to schedule the voicechat.\nToday is {thisday} {smonth} {year}. Chooosing a date preceeding today will be considered as next year {year+1}", reply_markup=InlineKeyboardMarkup(button))
 
 
@@ -129,12 +129,11 @@ async def start(client, message):
         return
     buttons = [
         [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot')
+            InlineKeyboardButton('✨ ꜱᴜᴘᴘᴏʀᴛ ✨', url='https://t.me/DarkPentester')
         ],
         [
-            InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help_main'),
-            InlineKeyboardButton('🗑 Close', callback_data='close'),
+            InlineKeyboardButton('➰ ʜᴇʟᴘ ➰', callback_data='help_main'),
+            InlineKeyboardButton('✗ ᴄʟᴏꜱᴇ ✗', callback_data='close'),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
@@ -148,19 +147,19 @@ async def show_help(client, message):
     reply_markup=InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("Play", callback_data='help_play'),
-                InlineKeyboardButton("Settings", callback_data=f"help_settings"),
-                InlineKeyboardButton("Recording", callback_data='help_record'),
+                InlineKeyboardButton("✗ ᴘʟᴀʏ ✗", callback_data='help_play'),
+                InlineKeyboardButton("✗ ꜱᴇᴛᴛɪɴɢꜱ ✗", callback_data=f"help_settings"),
+                InlineKeyboardButton("✗ ʀᴇᴄᴏʀᴅɪɴɢ ✗", callback_data='help_record'),
             ],
             [
-                InlineKeyboardButton("Scheduling", callback_data="help_schedule"),
-                InlineKeyboardButton("Controling", callback_data='help_control'),
-                InlineKeyboardButton("Admins", callback_data="help_admin"),
+                InlineKeyboardButton("✗ ꜱᴄʜᴇᴅᴜʟɪɴɢ ✗", callback_data="help_schedule"),
+                InlineKeyboardButton("✗ ᴄᴏɴᴛʀᴏʟɪɴɢ ✗", callback_data='help_control'),
+                InlineKeyboardButton("✗ ᴀᴅᴍɪɴꜱ ✗", callback_data="help_admin"),
             ],
             [
-                InlineKeyboardButton("Misc", callback_data='help_misc'),
-                InlineKeyboardButton("Config Vars", callback_data='help_env'),
-                InlineKeyboardButton("Close", callback_data="close"),
+                InlineKeyboardButton("✗ ᴍɪꜱᴄ ✗", callback_data='help_misc'),
+                InlineKeyboardButton("✗ ᴄᴏɴꜰɪɢ ᴠᴀʀꜱ ✗", callback_data='help_env'),
+                InlineKeyboardButton("✗ ᴄʟᴏꜱᴇ ✗", callback_data="close"),
             ],
         ]
         )
@@ -170,7 +169,7 @@ async def show_help(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(f"Help", url=f"https://telegram.dog/{Config.BOT_USERNAME}?start=help"),
+                        InlineKeyboardButton(f"➰ ʜᴇʟᴘ ➰", url=f"https://telegram.dog/{Config.BOT_USERNAME}?start=help"),
                     ]
                 ]
             ),)
@@ -188,15 +187,13 @@ async def show_help(client, message):
 async def repo_(client, message):
     buttons = [
         [
-            InlineKeyboardButton('🧩 Repository', url='https://github.com/subinps/VCPlayerBot'),
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),     
+            InlineKeyboardButton('✨ ꜱᴜᴘᴘᴏʀᴛ ✨', url='https://t.me/DarkPentester'),     
         ],
         [
-            InlineKeyboardButton("🎞 How to Deploy", url='https://youtu.be/mnWgZMrNe_0'),
-            InlineKeyboardButton('🗑 Close', callback_data='close'),
+            InlineKeyboardButton('✗ ᴄʟᴏꜱᴇ ✗', callback_data='close'),
         ]
     ]
-    await message.reply("<b>The source code of this bot is public and can be found at <a href=https://github.com/subinps/VCPlayerBot>VCPlayerBot.</a>\nYou can deploy your own bot and use in your group.\n\nFeel free to star☀️ the repo if you liked it 🙃.</b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+    await message.reply("<b>For More Information 👇🏻</b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
     await delete_messages([message])
 
 @Client.on_message(filters.command(['restart', 'update', f"restart@{Config.BOT_USERNAME}", f"update@{Config.BOT_USERNAME}"]) & admin_filter & chat_filter)
